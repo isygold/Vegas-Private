b325e85a75dc316be156ac2fc28094b8b8f9f560
isygold <israelagboola53@gmail.com>
2026-07-03 04:59:32 +0100

    feat(vegas): add isUnityGame() to detect Unity games on Winelator

--- src/dxvk/dxvk_vegas.h
+++ src/dxvk/dxvk_vegas.h
@@ -131,6 +131,9 @@ namespace dxvk {
 
     static uint64_t getSystemRamMB();
 
+    /** Returns true if the current process is a Unity game */
+    static bool isUnityGame();
+
     // ---- HW Masking (baked, not user-tunable) ----
 
     /** Apply VRAM scaling at config-load time (self-aware, Config overload) */
