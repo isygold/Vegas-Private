c046a1d905037cf48005d482a0d9a7cd24c35d0d
AGBOOLA ISRAEL OLUWAGBOGO <israelagboola53@gmail.com>
2026-07-28 14:10:17 +0100

    Revert "fix: update D3D11 swapchain tuneThreshold→adaptiveTune (missed in adaptive governor port)"
    This reverts commit a2268a14ce871a4bc0951dc8d799faf84376267e.

--- src/d3d11/d3d11_swapchain.cpp
+++ src/d3d11/d3d11_swapchain.cpp
@@ -368,7 +368,7 @@ namespace dxvk {
       m_lastPresentTime = now;
       float gpuLoad = (frameTime > 0.001f)
         ? std::min(frameTime / 16.667f, 1.0f) : 0.0f;
-      Vegas::adaptiveTune(gpuLoad, frameTime);
+      Vegas::tuneThreshold(gpuLoad, frameTime);
       Vegas::pushMetrics(gpuLoad, frameTime,
         VegasPerformanceState::Normal,
         Vegas::isFsrActive(), false);
